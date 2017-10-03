0b0f009688cd5141e2134b4c37872839f12997a1
Demidovich Ilya <ilyt23@mail.ru>
2017-10-03 22:11:42 +0300

    sorting layers + pause menu

---
+++ Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour {
+
+    public Canvas mainCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Event_Pause()
+    {
+        mainCanvas.sortingOrder = 2;
+        gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Event_Resume()
+    {
+        mainCanvas.sortingOrder = 1;
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Event_Exit()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
---
+++ Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+    private Renderer render;
+
+	// Use this for initialization
+	void Start () {
+        render = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 offset = new Vector2(0, Time.time * speed);
+
+        render.material.mainTextureOffset = offset;
+    }
+}
